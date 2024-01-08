01043b97c20f38eba17ed28f864c5c428f391b89
Ikramullah <ikramullahzx@gmail.com>
2024-01-08 19:39:03 +0800

    game/timer: add basic timer logic

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,5 +1,31 @@
 <script>
-  import * as Config from "$lib/config";
+  import Moment from "moment";
+  import { InitialTimer } from "$lib/config";
+
+  let enableTimer = true;
+  let targetTimer = InitialTimer;
+  let timer = Moment();
+
+  $: timerDiff = maxTimer.diff(timer);
+  $: maxTimer = Moment().add(targetTimer, 's');
+  $: timerPercentage = (timerDiff / (targetTimer * 1000)) * 100;
+
+  const incrementTimer = () => {
+    if (!enableTimer) return;
+    timer = Moment();
+    targetTimer += InitialTimer;
+  }
+
+  const timerInterval = () => setInterval(() => {
+    if (!enableTimer) return;
+    if (timerDiff <= 0) {
+      enableTimer = false;
+      alert("Lmao you lose.");
+    }
+
+    timer = Moment();
+  }, 1);
+  timerInterval();
 </script>
 
 <div class="h-screen w-screen max-w-screen p-10 bg-coffee-bg">
@@ -7,11 +33,11 @@
 
     <!-- Arena -->
     <div class="w-full h-[97%] flex">
-      <p class="m-auto text-5xl font-bold">Hello World!</p>
+      <p class="m-auto text-5xl font-bold" on:click={() => incrementTimer()}>Click to increment timer!</p>
     </div>
 
     <!-- Timer -->
-    <div class="w-full h-[3%] pb-3 px-7">
+    <div class="h-[3%] pb-3 px-7" style={`width: ${timerPercentage}%;`}>
       <div class="w-full h-full bg-coffee-border opacity-70 rounded-full"></div>
     </div>
 
