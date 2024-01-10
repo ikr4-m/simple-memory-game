69037f2e79979c983d00d511cbe07b6ca31fccb4
Ikramullah <ikramullahzx@gmail.com>
2024-01-10 13:57:06 +0800

    fix(app): preloading all assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script>
   import "$lib/app.css";
   import Game from "./Game.svelte";
+  import { CardList } from "$lib/config";
 </script>
 
 <svelte:head>
   <title>Simple Memory Game</title>
+  {#each CardList as card }
+    <link rel="preload" as="image" href={card.img}>
+  {/each}
 </svelte:head>
 <Game />
